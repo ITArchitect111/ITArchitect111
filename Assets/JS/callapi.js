98230aed1354bf22a696135ba659fec5a1f72e57
Chenczen C. Rafael <164977444+ITArchitect111@users.noreply.github.com>
2025-10-18 20:15:04 +0800

    Replace fetch API with EmailJS for email sending
    Refactored email sending logic to use EmailJS for form submission.

--- Assets/JS/callapi.js
+++ Assets/JS/callapi.js
@@ -1,26 +1,5 @@
-document.getElementById("contactForm").addEventListener("submit", async (e) => {
-      e.preventDefault();
-
-      const form = e.target;
-      const data = {
-        name: form.name.value,
-        email: form.email.value,
-        message: form.message.value
-      };
-
-      try {
-        const res = await fetch("/api/sendEmail", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
-        });
-
-        const result = await res.json();
-        alert(result.message);
-      } catch (err) {
-        alert("❌ Error sending message. Try again later.");
-        console.error(err);
-      }
-
-      form.reset();
-      });
+(function(){
+    emailjs.init({
+      publicKey: "F5QgPT3G9eBFCH1nQ", 
+    });
+  })();
